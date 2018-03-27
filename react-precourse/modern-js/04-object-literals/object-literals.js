a0783da90c12884660c6bdf991d0af30725e0577
Scott Van Gilder <scottvg@soundstrue.com>
2018-03-27 12:57:58 -0600

    add lesson 04-object-literals

---
+++ react-precourse/modern-js/04-object-literals/object-literals.js
@@ -0,0 +1,47 @@
+/* Computed Property Names */
+// Instead of this:
+// const id = 34
+
+// const users = {
+// }
+
+// users[id] = 'eliza'
+
+// You can use computed properties in Object literals, like so:
+// const id = 34
+
+// const users = {
+// 	34: 'eliza'
+// }
+
+// const newUsers = {
+// 	...users,
+// 	[id]: 'peggy'
+// }
+
+/* Shorthand Property Names */
+// Instead of this:
+// const id = 34,
+// 			name = 'eliza',
+// 			age = 23
+
+// const user = {
+// 	id: id,
+// 	name: name,
+// 	age: age,
+// }
+
+// You can use computed properties in Object literals, like so:
+// Notice the [id] bracket syntax in the object literal.
+const id = 34,
+			name = 'eliza',
+			age = 23
+
+const user = {
+	id,
+	name,
+	age,
+}
+
+console.log(user)
+// console.log(newUsers)
